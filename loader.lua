f36b82297ee26858ebfb202b732321ca82b98ca5
GTPS Resource Center <119905354+GTPS-Resource-Center@users.noreply.github.com>
2023-01-04 11:59:54 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,5 @@
-local file = "dexV4.lua" -- cache file name (workspace folder)
-local url = "https://raw.githubusercontent.com/loglizzy/dexV4/main/source.lua"
+local file = "dexV5.lua" -- cache file name (workspace folder)
+local url = "https://raw.githubusercontent.com/GTPS-Resource-Center/dex-test-v5/main/source.lua"
 
 local raw = isfile and isfile(file) and readfile(file)
 raw = raw or game:HttpGet(url)
